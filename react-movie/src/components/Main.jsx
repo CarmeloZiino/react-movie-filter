6a19db804a7fa7b08e11b40ae66411ac391a85c0
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-25 11:51:41 +0100

    Add: Repo

---
+++ react-movie/src/components/Main.jsx
@@ -0,0 +1,31 @@
+/*
+Dovrete utilizzare lo stato e useEffect per gestire il filtraggio dinamico.
+Per oggi diamo priorità alla logica e alla gestione dello stato. Una volta funzionante, possiamo pensare allo stile!
+
+Note
+
+il filtro deve funzionare dinamicamente quando l'utente seleziona un genere dalla select.
+Se non viene selezionato alcun genere, devono essere mostrati tutti i film.
+BONUS:
+Aggiungere un campo di ricerca per filtrare i film anche per titolo.
+Creare un sistema per aggiungere nuovi film alla lista tramite un form.*/
+import { useState, useEffect } from "react";
+
+const Main = () => {
+
+    const movie = [
+        { title: 'Inception', genre: 'Fantascienza' },
+        { title: 'Il Padrino', genre: 'Thriller' },
+        { title: 'Titanic', genre: 'Romantico' },
+        { title: 'Batman', genre: 'Azione' },
+        { title: 'Interstellar', genre: 'Fantascienza' },
+        { title: 'Pulp Fiction', genre: 'Thriller' },
+      ]
+
+    return(
+
+
+    )
+}
+
+export default Main;
